0ead95bc84e6685862102f0feb1556aaa12e6c80
Calvin Lang <langcalvin@gmail.com>
2015-11-10 13:18:29 +0800

    Finish 4.3 exercises

--- week-4/variable-methods.rb
+++ week-4/variable-methods.rb
@@ -1,3 +1,5 @@
+# Full name greeting: Write a program that asks for a person's first name, then middle, then last. 
+# Finally, it should greet the person using their full name.
 puts "What is your first name?"
 first_name = gets.chomp
 puts "What is your middle name?"
@@ -7,7 +9,9 @@ last_name = gets.chomp
 
 puts "Welcome #{first_name} #{middle_name} #{last_name}!"
 
+# Bigger, better favorite number: Write a program that asks for a person's favorite number. 
+# Have your program add 1 to the number, and then suggest the result as a bigger and better number.
 puts "What is your favorite number?"
 favorite_number = gets.chomp
 favorite_number_bigger = favorite_number.to_i + 1
-puts "Wouldn't you rather #{favorite_number}? It's bigger!"
+puts "Wouldn't you rather #{favorite_number_bigger}? It's bigger!" 
